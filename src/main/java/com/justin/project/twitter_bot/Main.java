6e9444591bd723066545380d4b312ea595716604
David <David@DESKTOP-ENDUEI1>
2019-10-30 23:08:26 -0400

    First Commit

---
+++ src/main/java/com/justin/project/twitter_bot/Main.java
@@ -0,0 +1,18 @@
+package com.justin.project.twitter_bot;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class Main {
+	public static void main(String[] args) {
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+		  .setOAuthConsumerKey("*********************")
+		  .setOAuthConsumerSecret("******************************************")
+		  .setOAuthAccessToken("**************************************************")
+		  .setOAuthAccessTokenSecret("******************************************");
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		Twitter twitter = tf.getInstance();
+	}
+}
